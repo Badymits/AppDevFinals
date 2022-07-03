e1d908652153e47e5b4f3d87b3374527b96b31b4
Badymits <71649205+Badymits@users.noreply.github.com>
2022-07-03 15:58:07 +0800

    initial commit in home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,1 +1,30 @@
 import 'package:flutter/material.dart';
+import 'contact.dart';
+import 'product.dart';
+import 'main.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Named Routes Demo',
+      // Start the app with the "/" named route. In this case, the app starts
+      // on the FirstScreen widget.
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        // '/': (context) => const contact(),
+        '/': (context) => const MyApp()
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        // '/second': (context) => const SecondScreen(),
+        //'/third': (context) => const ThirdScreen(),
+      },
+    ),
+  );
+}
+
+class CustomPageRoute extends MaterialPageRoute {
+  CustomPageRoute({builder}) : super(builder: builder);
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 0);
+}
