3bbc991ad80dde27ad428385388eddeed9112d8b
Badymits <71649205+Badymits@users.noreply.github.com>
2022-07-03 16:34:30 +0800

    added navigation file

---
+++ lib/navigation.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:finals_dev/home.dart';
+import 'package:finals_dev/product.dart';
+import 'package:finals_dev/contact.dart';
+
+class NavDrawer extends StatelessWidget {
+
+  String screen;
+
+  NavDrawer({Key? key, required this.screen}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ),
+            child: Text(
+              'Drawer Header',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.message),
+            title: const Text('First Screen'),
+            onTap: () {
+              Navigator.pop(context);
+              if(screen != '/'){
+                Navigator.pushNamed(context, '/');
+              }
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.account_circle),
+            title: const Text('Second Screen'),
+            onTap: () {
+              Navigator.pop(context);
+              if(screen != '/second') {
+                Navigator.pushNamed(context, '/second');
+              }
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Third Screen'),
+            onTap: () {
+              Navigator.pop(context);
+              if(screen != '/third') {
+                Navigator.pushNamed(context, '/third');
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
