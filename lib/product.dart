e47edfeea28e43ba6c66d388a4380e50ab856e22
KuyaKing0601 <king.delarosa@ciit.edu.ph>
2022-07-04 18:19:30 +0800

    Added one image pruduct and one purchased button etc..

--- lib/product.dart
+++ lib/product.dart
@@ -29,8 +29,71 @@ class product extends StatelessWidget {
         backgroundColor: primaryColor,
       ),
 
-      body: const Center(
-          child: Text('Product Page')
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              child: const Text(
+                  'WHAT WE OFFER',
+                   style: TextStyle(
+                       fontSize: 30,
+                   )),
+            ),
+            Container(
+              height: 100,
+              width:400,
+              alignment: Alignment.center,
+              child: const Text(
+                  'KAE is the health consumer app that ultimately strives to give the best and trusted suplemments there is. We Intend to support and aid in mainting your health! We Choose our supplements very seriusly. Heres whats in store for you all..',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16,)
+              ),
+            ),
+            Container(
+              margin: new EdgeInsets.symmetric(vertical: 20.0),
+              width: 350,
+              height: 250,
+              decoration: BoxDecoration(
+                color: const Color(0xff7c94b6),
+                image: const DecorationImage(
+                  image: NetworkImage(
+                      'https://assets.unilab.com.ph/uploads/Common/Products/Alnix%20Plus%20Syrup-485/Alnix%20Plus%20Syrup-816.jpg'),
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                border: Border.all(
+                  color: Color(0xFFe0e0e0),
+                  width: 4.0,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: const Text(
+                  'Alnix Plus Syrup',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                  )),
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 12,
+                ),
+                ElevatedButton.icon(   // <-- ElevatedButton
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.shopping_cart,
+                    size: 25.0,
+                  ),
+                  label: Text('PURCHASE NOW!'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
       drawer: NavDrawer(screen: '/'),
     );
